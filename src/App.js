1f62d9020ef7d1c03a154031f26c870f7a1142be
Abdullah Bashir <abdullahbr98@gmail.com>
2023-06-05 13:03:13 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login.jsx";
+import SignUp from "./pages/SignUp/Signup.jsx";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path= '/signup' exact element={<SignUp/>} />
+                <Route path= '/login' exact element={<Login/>} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/classes/AccountClass.js
@@ -0,0 +1,19 @@
+class AccountClass{
+    #emailAddress;
+    #password;
+
+    constructor(emailAddress,password){
+        this.emailAddress = emailAddress;
+        this.password = password;
+    }
+
+    set setEmail(email){
+        this.emailAddress = email;
+    }
+
+    set setPassword(password){  
+        this.password = password;
+    }
+}
+
+export default AccountClass;
